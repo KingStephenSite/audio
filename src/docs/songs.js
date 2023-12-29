e70ec0ee9917583b40e79d2516e67aada8c6ce8d
Mr. Rainbow <88560300+DaRealMrRainbow@users.noreply.github.com>
2023-12-29 14:08:42 -0500

    e

---
+++ src/docs/songs.js
@@ -0,0 +1,153 @@
+export const song_list = [
+  {
+    title: 'Below the Surface',
+    artistName: 'Griffinilla',
+    img: 'belowthesurface.jpg',
+    fileUrl: '/music/below_the_surface.mp3',
+    musicIns: '',
+    letra: `[00:00.32] Listen close
+    [00:02.02] Follow my instructions
+    [00:04.92] There is no
+    [00:06.89] Time for introductions
+    [00:09.37] He was the one that made us
+    [00:14.05] You'll be the one to save us
+    [00:18.84] Underground
+    [00:20.11] Welcome to the circus
+    [00:23.15] Power down
+    [00:24.90] Are you feeling nervous?
+    [00:27.98] His voice means to deceive you
+    [00:32.26] My voice just wants to lead you
+    [00:39.98] Below the surface
+    [00:44.24] Built without purpose
+    [00:49.08] Did we deserve this?
+    [00:53.22] You're here to serve
+    [00:55.15] Daddy please!
+    [00:56.96] We all scream for ice cream
+    [00:59.91] This machine
+    [01:01.15] Will help with the brain freeze
+    [01:04.36] The stage lights up with controlled shocks
+    [01:08.93] So tighten up all your spring locks
+    [01:13.38] Through the vent
+    [01:15.05] Keeping your composure
+    [01:18.09] Hold your breath
+    [01:19.42] Something's creeping closer
+    [01:22.84] There's no one left to find you
+    [01:27.08] I'll take your place inside you
+    [01:31.28] 
+    [01:34.45] Below the surface
+    [01:39.13] Built without purpose
+    [01:43.73] Did we deserve this?
+    [01:48.28] You're here to serve us
+    [01:52.41] 
+    [01:54.75] I'll take your place
+    [01:56.95] Behind the mask
+    [01:59.67] Then I'll be first
+    [02:01.32] And you'll be last
+    [02:03.91] Yeah, you'll be last
+    [02:10.21] 
+    [02:11.93] 
+    `,
+  },
+  {
+    title: 'Golden Hour',
+    artistName: 'JVKE',
+    img: 'goldenhour.jpg',
+    fileUrl: '/music/goldenhour.mp3',
+    musicIns: '',
+    letra: `[00:00.00]
+    [00:15.31] It was just two lovers, sittin' in the car, listening to Blonde
+    [00:18.83] Fallin' for each other, pink and orange skies, feelin' super childish
+    [00:23.27] No Donald Glover, missed call from my mother
+    [00:26.75] Like, "Where you at tonight?" Got no alibi
+    [00:28.82] I was all alone with the love of my life
+    [00:33.72] She's got glitter for skin, my radiant beam in the night
+    [00:40.88] I don't need no light to see you
+    [00:46.09] Shine
+    [00:50.10] It's your golden hour (oh-ooh-oh)
+    [00:58.43] You slow down time
+    [01:05.16] In your golden hour (oh-ooh-oh)
+    [01:16.56] We were just two lovers, feet up on the dash, drivin' nowhere fast
+    [01:20.30] Burnin' through the summer, radio on blast, make the moment last
+    [01:24.22] She got solar power, minutes feel like hours
+    [01:27.98] She knew she was the baddest, can you even imagine
+    [01:29.98] Fallin' like I did for the love of my life?
+    [01:34.86] She's got glow on her face, a glorious look in her eyes
+    [01:42.03] My angel of light
+    [01:45.13] I was all alone with the love of my life
+    [01:50.14] She's got glitter for skin, my radiant beam in the night
+    [01:57.27] I don't need no light to see you
+    [02:02.35] Shine
+    [02:06.19] It's your golden hour (oh-ooh-oh)
+    [02:14.68] You slow down time
+    [02:21.33] In your golden hour (oh-ooh-oh)
+    [02:27.06]`
+  },
+{
+title: 'Running Up That Hill',
+  artistName: 'Kate Bush',
+    img: 'ruth-kb.jpg',
+      fileUrl: '/music/ruth-kb.mp3',
+        musicIns: '',
+          letra: `[00:00.00]
+  [00:24.95]It doesn't hurt me
+  [00:27.98]Do you want to feel how it feels?
+  [00:32.14]Do you want to know, know that it doesn't hurt me?
+  [00:36.66]Do you want to hear about the deal that I'm making?
+  [00:41.25]You, it's you and me
+  [00:49.92]And if I only could
+  [00:51.67]I'd make a deal with God
+  [00:53.80]And I'd get him to swap our places
+  [00:57.51]Be running up that road
+  [00:59.53]Be running up that hill
+  [01:01.65]Be running up that building
+  [01:06.17]See if I only could, oh
+  [01:22.12]You don't want to hurt me
+  [01:25.74]But see how deep the bullet lies
+  [01:29.99]Unaware I'm tearing you asunder
+  [01:34.24]Ooh, there is thunder in our hearts
+  [01:39.01]Is there so much hate for the ones we love?
+  [01:43.17]Tell me, we both matter, don't we?
+  [01:47.69]You, it's you and me
+  [01:56.39]It's you and me, won't be unhappy
+  [02:00.42]And if I only could
+  [02:02.72]I'd make a deal with God
+  [02:04.76]And I'd get him to swap our places
+  [02:08.21]Be running up that road
+  [02:10.33]Be running up that hill
+  [02:12.46]Be running up that building
+  [02:16.93]Say, if I only could, oh
+  [02:34.63]You
+  [02:38.26]It's you and me
+  [02:42.92]It's you and me, won't be unhappy
+  [02:47.60]Oh come on, baby
+  [02:49.35]Oh come on, darling
+  [02:52.02]Let me steal this moment from you now
+  [02:56.27]Oh come on, angel
+  [02:58.08]Come on, come on, darling
+  [03:00.68]Let's exchange the experience, oh
+  [03:21.03]And if I only could
+  [03:23.33]I'd make a deal with God
+  [03:25.62]And I'd get him to swap our places
+  [03:29.60]I'd be running up that road
+  [03:32.26]Be running up that hill
+  [03:34.91]With no problems
+  [03:38.97]Said, if I only could
+  [03:41.00]I'd make a deal with God
+  [03:43.40]And I'd get him to swap our places
+  [03:47.91]I'd be running up that road
+  [03:49.92]Be running up that hill
+  [03:52.31]With no problems
+  [03:56.98]So if I only could
+  [03:59.01]I'd make a deal with God
+  [04:01.41]And I'd get him to swap our places
+  [04:05.66]I'd be running up that road
+  [04:07.69]Be running up that hill
+  [04:10.08]With no problems
+  [04:14.59]Said, if I only could
+  [04:19.01]I'd be running up that hill
+  [04:23.26]With no problems
+  [04:34.32](If I only could, I'd be running up that hill)
+  [04:39.10](If I only could, I'd be running up that hill)`
+}
+
+]
